db65a06fa22edb6bd27af6a572c62e920af00403
Narendra Prajapati <arcade.narendra@gmail.com>
2026-07-31 02:19:56 +0530

    Add health check endpoint

--- server.js
+++ server.js
@@ -6,3 +6,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+console.log("MAIL:", process.env.MAIL);
+console.log("PASS exists:", !!process.env.PASS);
